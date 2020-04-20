7364ce522bec3a127fbbbac9e2915b3ed6a54236
Thales Silva <sthales2@icloud.com>
2020-04-20 16:37:31 -0300

    Fix from https://github.com/Rhymen/go-whatsapp/pull/332

--- read.go
+++ read.go
@@ -28,7 +28,10 @@ func (wac *Conn) readPump() {
 	for {
 		readerFound := make(chan struct{})
 		go func() {
-			msgType, reader, readErr = wac.ws.conn.NextReader()
+			// msgType, reader, readErr = wac.ws.conn.NextReader()
+			if wac.ws != nil {
+				msgType, reader, readErr = wac.ws.conn.NextReader()
+			}
 			close(readerFound)
 		}()
 		select {
