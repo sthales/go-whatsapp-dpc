dce3b84b8a0771902d30db03a2e150387d0f17a1
Thales Silva <souzaathales@gmail.com>
2021-06-17 09:52:06 -0300

    Update message.go

--- message.go
+++ message.go
@@ -69,6 +69,9 @@ func (wac *Conn) Send(msg interface{}) (string, error) {
 		return "ERROR", fmt.Errorf("cannot match type %T, use message types declared in the package", msg)
 	}
 
+	status := proto.WebMessageInfo_PENDING
+	msgProto.Status = &status
+	
 	ch, err := wac.sendProto(msgProto)
 	if err != nil {
 		return "ERROR", fmt.Errorf("could not send proto: %v", err)
